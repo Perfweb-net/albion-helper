32c1ae7b2c07b344d8187e7632e4b0b1e31f4d94
Pierre Saugues <psaugues@MacBook-Pro-de-Pierre-2.local>
2025-02-18 16:04:36 +0100

    v2.1

--- front/src/layout/Header.jsx
+++ front/src/layout/Header.jsx
@@ -1,59 +1,55 @@
 import React, {useContext, useState} from 'react';
-import {AppBar, Toolbar, Button, Box} from '@mui/material';
+import {AppBar, Toolbar, Button, Box, IconButton, Drawer, Divider} from '@mui/material';
 import {Link, useNavigate} from 'react-router-dom';
 import {UserContext} from "../context/UserContext";
 import Logo from "../components/svg/Logo";
 import {motion} from "framer-motion";
-
+import MenuIcon from '@mui/icons-material/Menu'; // Icône du burger menu
+import CloseIcon from '@mui/icons-material/Close'; // Icône de la croix pour fermer le drawer
 
 const Header = () => {
-    // L'état de l'utilisateur connecté
-    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('token') !== null);
     const navigate = useNavigate();
     const {isLogin, setIsLogin} = useContext(UserContext);
+
+    // Gestion de l'état du menu burger
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const handleLogout = () => {
         localStorage.removeItem('token'); // Suppression du token
         localStorage.removeItem('refreshToken');
-        setIsAuthenticated(false); // Mise à jour de l'état de connexion
         setIsLogin(false);
         navigate('/login'); // Rediriger vers la page de login
     };
 
-    return (
-        <AppBar position="sticky">
-            <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
-                {/* Logo à gauche */}
-                <Box sx={{cursor: "pointer"}} onClick={() => navigate('/dashboard')}>
-                    <motion.div
-                        initial={{ x: 0, rotate: 0 }}
-                        animate={{ x: 0, rotate: 0 }}
-                        whileHover={{
-                            x: [0, -2, 2, -2, 2, 0], // Mouvement de vibration
-                            rotate: [0, -2, 2, -2, 2, 0], // Légère rotation
-                            transition: { duration: 1, repeat: Infinity }
-                        }}
-                    >
-                        <Logo color={"white"} width={90} height={90}/>
-                    </motion.div>
-                </Box>
+    const toggleDrawer = (open) => {
+        setIsDrawerOpen(open);
+    };
 
-                {/* Menu au centre */}
+    const drawerContent = (
+            <Box sx={{width: 250, display: 'flex', flexDirection: 'column', gap: 2, padding: 2}}>
+                {/* Icône pour fermer le drawer */}
+                <IconButton onClick={() => toggleDrawer(false)} sx={{alignSelf: 'flex-end'}}>
+                    <CloseIcon/>
+                </IconButton>
+
+                {/* Menu items */}
                 {isLogin && (
-                    <Box sx={{display: 'flex', gap: 3, marginRight: 4, justifyContent: "flex-end"}}>
+                    <>
                         <Button color="inherit" component={Link} to="/guilds">Guilds</Button>
+                        <Divider/>
                         <Button color="inherit" component={Link} to="/players">Players</Button>
+                        <Divider/>
                         <Button color="inherit" component={Link} to="/map">Map</Button>
-                    </Box>
+                        <Divider/>
+                    </>
                 )}
 
-                {/* Login / Logout à droite */}
-                <Box>
-                    {isLogin ? (
+                {
+                    isLogin ? (
                         <Button
-                            style={{backgroundColor: "white", color: "black"}} // Couleur bleue pour login
-                            color="primary" // Couleur bleue pour login
-                            variant="contained" // Style de bouton plein
+                            style={{backgroundColor: "white", color: "black"}}
+                            color="primary"
+                            variant="contained"
                             onClick={handleLogout}
                         >
                             Logout
@@ -61,9 +57,9 @@ const Header = () => {
                     ) : (
                         <div style={{display: "flex", gap: 10}}>
                             <Button
-                                style={{backgroundColor: "white", color: "black"}} // Couleur bleue pour login
-                                color="primary" // Couleur bleue pour login
-                                variant="contained" // Style de bouton plein
+                                style={{backgroundColor: "white", color: "black"}}
+                                color="primary"
+                                variant="contained"
                                 component={Link}
                                 to="/register"
                             >
@@ -71,9 +67,84 @@ const Header = () => {
                             </Button>
 
                             <Button
-                                style={{backgroundColor: "#787a7d", color: "white"}} // Couleur bleue pour login
-                                color="secondary" // Couleur bleue pour login
-                                variant="contained" // Style de bouton plein
+                                style={{backgroundColor: "#787a7d", color: "white"}}
+                                color="secondary"
+                                variant="contained"
+                                component={Link}
+                                to="/login"
+                            >
+                                Login
+                            </Button>
+                        </div>
+                    )
+                }
+            </Box>
+        )
+    ;
+
+    return (
+        <AppBar position="sticky">
+            <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
+                {/* Logo à gauche */}
+                <Box sx={{cursor: "pointer"}} onClick={() => navigate('/dashboard')}>
+                    <motion.div
+                        initial={{x: 0, rotate: 0}}
+                        animate={{x: 0, rotate: 0}}
+                        whileHover={{
+                            x: [0, -2, 2, -2, 2, 0], // Mouvement de vibration
+                            rotate: [0, -2, 2, -2, 2, 0], // Légère rotation
+                            transition: {duration: 1, repeat: Infinity}
+                        }}
+                    >
+                        <Logo color={"white"} width={90} height={90}/>
+                    </motion.div>
+                </Box>
+
+                {/* Menu burger pour les petits écrans */}
+                <Box sx={{display: {xs: 'block', sm: 'none'}}}>
+                    <IconButton color="inherit" onClick={() => toggleDrawer(true)}>
+                        <MenuIcon/>
+                    </IconButton>
+                </Box>
+
+                {/* Menu central pour les grands écrans */}
+                <Box sx={{display: {xs: 'none', sm: 'flex'}, gap: 3, marginRight: 4, justifyContent: "center"}}>
+                    {isLogin && (
+                        <>
+                            <Button color="inherit" component={Link} to="/guilds">Guilds</Button>
+                            <Button color="inherit" component={Link} to="/players">Players</Button>
+                            <Button color="inherit" component={Link} to="/map">Map</Button>
+                        </>
+                    )}
+                </Box>
+
+                {/* Login / Logout à droite */}
+                <Box sx={{display: {xs: 'none', sm: 'flex'}}}>
+                    {isLogin ? (
+                        <Button
+                            style={{backgroundColor: "white", color: "black"}}
+                            color="primary"
+                            variant="contained"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </Button>
+                    ) : (
+                        <div style={{display: "flex", gap: 10}}>
+                            <Button
+                                style={{backgroundColor: "white", color: "black"}}
+                                color="primary"
+                                variant="contained"
+                                component={Link}
+                                to="/register"
+                            >
+                                Register
+                            </Button>
+
+                            <Button
+                                style={{backgroundColor: "#787a7d", color: "white"}}
+                                color="secondary"
+                                variant="contained"
                                 component={Link}
                                 to="/login"
                             >
@@ -83,6 +154,15 @@ const Header = () => {
                     )}
                 </Box>
             </Toolbar>
+
+            {/* Drawer pour mobile */}
+            <Drawer
+                anchor="left"
+                open={isDrawerOpen}
+                onClose={() => toggleDrawer(false)}
+            >
+                {drawerContent}
+            </Drawer>
         </AppBar>
     );
 };
